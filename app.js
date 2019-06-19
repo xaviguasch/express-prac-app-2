c4fd5d479f0e4861d31553b7d48bd7d7da4f0fcc
xaviguasch <xaviguasch@gmail.com>
2019-06-19 15:39:52 +0200

    added solution

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const express = require('express')
 
 const app = express()
 
-const usersRoutes = require('./routes/users')
-const homeRoutes = require('./routes/home')
+const mainRoutes = require('./routes/index')
 
 app.get('/favicon.ico', (req, res) => res.status(204)) // boilerplate code, conflict with favicon
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/users', usersRoutes)
-app.use('/', homeRoutes)
+app.use(mainRoutes)
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const express = require('express')
+
+const rootDir = require('../util/path')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'index.html'))
+})
+router.get('/users', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'users.html'))
+})
+
+module.exports = router
